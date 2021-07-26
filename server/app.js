32f1674e71550340a0e360d758a44c163133f79d
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-07-26 16:51:38 +0500

    add
    add

--- server/app.js
+++ server/app.js
@@ -1,55 +1,58 @@
-const express = require( "express" );
-const config = require( "config" );
-const serveIndex = require( "serve-index" );
-const morgan = require( "morgan" );
-const { userRouter } = require( "../src/routes/user.routes" );
-const { logger } = require( "../src/logger/logger" );
-const winston = require( "winston" );
-const { errorHandler } = require( "../src/errors/errorHandler" );
-const { connectDB } = require( "../connection/connect" );
-const { authRouter } = require( "../src/routes/auth.routes" );
-const path = require( "path" );
-const { productRouter } = require( "../src/routes/product.routes" );
+const express = require("express");
+const config = require("config");
+const serveIndex = require("serve-index");
+const morgan = require("morgan");
+const { userRouter } = require("../src/routes/user.routes");
+const { logger } = require("../src/logger/logger");
+const winston = require("winston");
+const { errorHandler } = require("../src/errors/errorHandler");
+
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const redis = require("redis");
+
+const { authRouter } = require("../src/routes/auth.routes");
+const path = require("path");
+const { productRouter } = require("../src/routes/product.routes");
+
 // Create App server
 const app = express();
 
 // Access json
-app.use( express.json( { extended: true } ) );
-app.use( express.urlencoded( { extended: false } ) );
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 // Static files
 app.use(
     "/public",
-    express.static( "public" ),
-    serveIndex( "public", { icons: true } )
+    express.static("public"),
+    serveIndex("public", { icons: true })
 );
 
 // Static files other way
-app.use( "/public", express.static( path.join( __dirname, "public" ) ) );
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 // Show Requests to console
-if ( app.get( "env" ) === "development" ) {
-    app.use( morgan( "tiny" ) );
+if (app.get("env") === "development") {
+    app.use(morgan("tiny"));
     // Write log
     logger.add(
-        new winston.transports.Console( {
+        new winston.transports.Console({
             format: winston.format.simple(),
-        } )
+        })
     );
 }
 
 // Api Routes
-app.use( "/api/user", userRouter );
-app.use( "/api/auth", authRouter );
-app.use( "/api/product", productRouter )
-
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/product", productRouter);
 
 // Error Handler
 // Please put this code after last middleware
-app.use( errorHandler );
+app.use(errorHandler);
 
 // Create PORT
-const PORT = config.get( "PORT" ) || process.env.PORT || 5000;
+const PORT = config.get("PORT") || process.env.PORT || 5000;
 
-
-app.listen( PORT, () => console.log( `Server is running on ${ PORT }` ) );
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
